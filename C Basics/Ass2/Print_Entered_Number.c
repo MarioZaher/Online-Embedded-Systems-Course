d0bedd7cfd6f7cb7465f0ba25e68f53ae85ef53d
Mario zaher <eng.mario.zaher@gmail.com>
2021-02-23 10:08:48 +0200

    Print Entered Number

---
+++ C Basics/Ass2/Print_Entered_Number.c
@@ -0,0 +1,22 @@
+/*
+ * Print_Entered_Number.c
+ *
+ *  Created on: Feb 21, 2021
+ *      Author: Mario
+ */
+
+#include<stdio.h>
+
+int main()
+{
+
+	int num;
+	printf("Enter The Number : \n");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&num);
+	printf("you Entered The Number : %d",num);
+
+
+
+return 0;
+}
