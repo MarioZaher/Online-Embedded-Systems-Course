225130ca9082716c201a753b0fcc38b358d8aeee
Mario zaher <eng.mario.zaher@gmail.com>
2021-02-23 10:10:46 +0200

    Add Two Numbers

---
+++ C Basics/Ass3/Add_Two_Num.c
@@ -0,0 +1,20 @@
+/*
+ * Add_Two_Num.c
+ *
+ *  Created on: Feb 21, 2021
+ *      Author: Mario
+ */
+
+
+#include<stdio.h>
+int main()
+{
+
+	int num1,num2;
+	printf("Please Enter Two Numbers\n");
+	fflush(stdin);fflush(stdout);
+	scanf("%d %d",&num1,&num2);
+	printf("The sum of Two Number = %d", num1+num2);
+	return 0;
+
+}
